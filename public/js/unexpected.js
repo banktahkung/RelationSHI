3e82b25b45772fd92d0e8302d458d9fb7dd67999
banktahkung <banktahkung@gmail.com>
2025-02-11 00:42:59 +0700

    Unexpected update [real]

--- public/js/unexpected.js
+++ public/js/unexpected.js
@@ -1,23 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const Container = document.getElementById("Container");
+document.addEventListener("DOMContentLoaded", function () {
+  const Container = document.getElementById("Container");
 
-    Container.style.top = "0vh";
-})
+  Container.style.top = "0vh";
 
-async function GetPopularity(){
-    const response = await fetch("/popularity");
-    const data = await response.json();
+  A();
+});
 
-    return data.pop;
+async function GetPopularity() {
+  const response = await fetch("/popularity");
+  const data = await response.json();
+
+  return data.pop;
 }
 
-setInterval(async function(){
-    await A();
+setInterval(async function () {
+  await A();
 }, 10000);
 
-async function A(){
-    const data = await GetPopularity();
-    const Popularity = document.querySelector(".PopularityText");
+async function A() {
+  const popularity = await GetPopularity();
+  const Popularity = document.querySelector(".PopularityText");
 
-    Popularity.innerHTML = " 🤝 ในขณะนี้มีคนอยากรู้จักคุณ : " + (popularity? popularity : 0);
+  Popularity.innerHTML =
+    " 🤝 ในขณะนี้มีคนอยากรู้จักคุณ : " + (popularity ? popularity : 0);
 }
